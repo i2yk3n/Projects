24d7789e9ffefeabf2120d37c8ed3a6b9a95a62f
i2yk3n <Kinjeseto@gmail.com>
2026-08-24 10:13:02 -0500

    Added first Project to test GIthub

---
+++ First.Java/src/First.java
@@ -0,0 +1,16 @@
+//Name:Dylan Huguley
+public class First
+{
+    public static void main(String[] args)
+    {
+        /* Code Starts here */
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Software Developer");
+        System.out.println("Why: I like money");
+        System.out.println("Median Salary: 135,650");
+
+
+
+    }
+}
+
