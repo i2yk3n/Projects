40f50be371972a791ae25bc37f966ae021760320
i2yk3n <Kinjeseto@gmail.com>
2026-08-30 12:08:14 -0500

    Averages Hard coded test Results
    Module 2 Assignment 1

---
+++ Test/src/Test.java
@@ -0,0 +1,20 @@
+//Dylan Huguley
+public class Test {
+    public static void main(String[] args){
+        //declare variables
+        double test1;
+        double test2;
+        double test3;
+        double avgTest;
+        //hard coded test results
+        test1=88.2;
+        test2=78.9;
+        test3=97.6;
+        avgTest=(test1+test2+test3)/3;
+        //print results & average
+        System.out.println("Test score 1: " +test1);
+        System.out.println("Test score 2: " +test2);
+        System.out.println("Test score 3: " +test3);
+        System.out.println("The average of 3 test scores is: " +avgTest);
+    }
+}
